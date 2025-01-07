3b7bf4caebd365d5cd6ed5812e727ca37d8569c5
Bohaska <73286691+Bohaska@users.noreply.github.com>
2025-01-07 14:07:50 +0800

    remove examples from nightgfx.py

--- nightgfx.py
+++ nightgfx.py
@@ -178,17 +178,3 @@ def process_image(input_image_path, automatic_lights=None, output_mask_path=None
         output_image_path = input_image_path + "--32bpp.png"
     bpp_result_img.save(output_image_path)
     print(f"Processed 32bpp saved to {output_image_path}")
-
-# Example usage
-input_image_path = input('Input image path of object\n(example: infra06, infra06.png)\n> ')  # Provide the input image path
-
-process_image(input_image_path)
-
-# Example automatic conversion
-input_folder_path = input('Input folder name of images to convert\n> ')
-from os import listdir, mkdir
-from os.path import isfile, join
-onlyfiles = [f for f in listdir(input_folder_path) if isfile(join(input_folder_path, f)) and f[-4:].lower() == ".png"]
-mkdir(f"{input_folder_path}/nightgfx")
-for file in onlyfiles:
-    process_image(f"{input_folder_path}/{file}", automatic_lights=True, output_image_path=f"{input_folder_path}/nightgfx/{file[:-4]}")
